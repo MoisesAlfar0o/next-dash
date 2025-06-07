b216d3910530ac4a8d59b7d283bd7f751a6b646e
Moises Alfaro <107770920+MoisesAlfar0o@users.noreply.github.com>
2025-06-07 13:12:56 -0600

    Add files via upload

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+// import { Inter } from 'next/font/google' <- vercel
+import { Inter, Montserrat } from 'next/font/google'
+
+export const montserrat = Montserrat({ subsets: ['latin']})
+
+export const inter = Inter({ subsets: ['latin']})
